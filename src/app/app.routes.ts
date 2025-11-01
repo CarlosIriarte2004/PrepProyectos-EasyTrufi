8d5b03bd4bd0c81f1e7f5bd89be966d4993f7df2
Carlitos <carlos.iriarte@ucb.edu.bo>
2025-11-01 18:56:37 -0400

    feat(routes): configurar enrutamiento standalone y bootstrap de AppComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./app/features/home/pages/inicio/inicio')
+        .then(({ InicioComponent }) => InicioComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./app/features/auth/pages/login/login')
+        .then(({ LoginComponent }) => LoginComponent)
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./app/features/auth/pages/registro/registro')
+        .then(({ RegistroComponent }) => RegistroComponent)
+  },
+  { path: '**', redirectTo: '' }
+];
