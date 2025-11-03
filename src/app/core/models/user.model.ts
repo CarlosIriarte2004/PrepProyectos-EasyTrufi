fe7fd947dc8125c1e561938997dae2a13d5ef327
Carlitos <carlos.iriarte@ucb.edu.bo>
2025-11-03 16:24:31 -0400

    Proyecto finalizado

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,25 @@
+// src/app/core/models/user.model.ts
+export type Beneficio = 'ninguno' | 'estudiantil' | 'tercera_edad' | 'discapacidad';
+
+export interface EasyTrufiUser {
+  id: string;
+  nombre: string;
+  email: string;
+  password?: string;
+  uidTarjeta: string;
+  saldo: number;
+  beneficio: Beneficio;
+  createdAt: string;
+  loginAt?: string;
+}
+
+/** Payload para crear usuario en MockAPI */
+export interface UserDTO {
+  nombre: string;
+  email: string;
+  password: string;
+  uidTarjeta: string;
+  saldo: number;
+  beneficio: Beneficio;
+  createdAt?: string;
+}
